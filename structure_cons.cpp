2b597f45f206fdef6af12e7e2ccd52afbd7014ea
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-04-02 19:17:23 +0530

    Add files via upload

---
+++ structure_cons.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+struct abc
+{
+	int a1;
+	string name;
+}
+s1;
+int main()
+{
+	s1.a1=101;
+	s1.name="abc";
+	cout<<s1.a1<<endl;
+	cout<<s1.name<<endl;
+	return 0;
+}
